20de9269e8cc2d136b7e9a0eeaaf29f903087a5b
pilpod <giacomo.corso@gmail.com>
2021-03-07 15:57:38 +0100

    function oncalculateclick added, app working

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -3,22 +3,34 @@ using Android.App;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
-using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
+using Android.Widget;
 
 namespace TipCalculator
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
 
         }
 
@@ -44,6 +56,20 @@ namespace TipCalculator
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void OnCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+            double bill = 0;
+            if(double.TryParse(text, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
+            }
         }
 	}
 }
